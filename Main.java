298ce8babb3687c6032bdf6a2443b2c9fdc1aec1
Marta Hermida <marta.colacao@gmail.com>
2025-11-09 16:04:30 +0100

    Actualiza mensaje en Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Versión inicial del proyecto.");
+        System.out.println("Segunda versión del proyecto.");
     }
 }
