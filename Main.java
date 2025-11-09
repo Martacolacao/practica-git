ecd714037e53419d8bf9d907d1393ab91f7b382e
Marta Hermida <marta.colacao@gmail.com>
2025-11-09 16:42:06 +0100

    feature: Implementación de registro

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Segunda versión del proyecto.");
+        System.out.println("Versión inicial del proyecto.");
+        System.out.println("Funcionalidad de registro implementada.");
     }
 }
